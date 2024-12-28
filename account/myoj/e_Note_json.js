ef21efec547861c635ed8870c0a7805eb5b5bae5
root <root@ip-172-31-87-112.ec2.internal>
2024-12-28 16:24:51 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -9795,7 +9795,7 @@ var _bnotes=
             "15": "",
             "16": "241228_024826(ed).<ol><li>Politics and faith relations </li><li>State and Church relationship</li><li>If Trump falls, what shall we do?</li><li>New</li></ol>",
             "17": "241228_112413(ed).<ol><li>Endurance</li><li><br></li></ol>",
-            "18": "",
+            "18": "241228_112450(ed).<ol><li>Rebel</li></ol>",
             "19": "241226_102146(ed).<ol><li>560 BC. 512Bc. </li><li>Is rebellion good or bad? </li><li>Deny ourselves culture </li></ol>",
             "20": "",
             "21": "",
