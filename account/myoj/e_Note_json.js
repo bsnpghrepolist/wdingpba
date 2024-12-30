942ab353ccfd0e0d625d62903beda88841b18d3a
root <root@ip-172-31-87-112.ec2.internal>
2024-12-30 14:33:49 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -9792,7 +9792,7 @@ var _bnotes=
             "12": "",
             "13": "",
             "14": "",
-            "15": "241230_093230(ed).<ol><li>Bad Relations with people.</li><li>President Campaign. Flater the voters. Cheat the voters. Make use of voters. Learn voters.</li><li>Christian private protection.  </li></ol>",
+            "15": "241230_093345(ed).<ol><li>Bad Relations with people.</li><li>President Campaign. Flater the voters. Cheat the voters. Make use of voters. Learn voters.</li><li>Christian private protection.  </li><li>CCP: Serve the people and soul. </li></ol>",
             "16": "241228_112520(ed).<ol><li>Politics and faith relations </li><li>State and Church relationship</li><li>If Trump falls, what shall we do?</li><li>Escape</li></ol>",
             "17": "241228_112413(ed).<ol><li>Endurance</li><li><br></li></ol>",
             "18": "241228_112758(ed).<ol><li>Rebel, fight</li></ol>",
