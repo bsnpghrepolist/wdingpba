ae9c221d81532d2162a9ba7eaf622d920f8b7c27
root <root@ip-172-31-24-87.ec2.internal>
2025-03-31 13:42:07 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -10565,7 +10565,7 @@ var _bnotes=
             "17": "",
             "18": "",
             "19": "",
-            "20": "",
+            "20": "250331.094205(ed).<ol><li>resurrected people will die again. </li><li>miracle for dead; </li></ol>",
             "21": "",
             "22": "",
             "23": "",
