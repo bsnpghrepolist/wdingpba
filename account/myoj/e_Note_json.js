86d9b03059651d934b8aa3657f70f977a2636c14
root <root@ip-172-31-24-87.ec2.internal>
2025-08-08 13:50:19 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -12129,7 +12129,7 @@ var _bnotes=
             "4": "",
             "5": "",
             "6": "",
-            "7": "250808.094751(ed).<ol><li>Solomon is the offspring of the Hittites.</li></ol>",
+            "7": "250808.095016(ed).<ol><li>Solomon is the offspring of the Hittites.</li><li>Jebusites, the original owners of the land of the Temple. </li></ol>",
             "8": "",
             "9": "",
             "10": "",
